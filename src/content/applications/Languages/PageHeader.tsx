1366dde3d7a4afc14963eec726b6eabebaa553de
maradox49 <maradox49ss@gmail.com>
2023-12-06 20:56:17 +0100

    langauge modal done

--- src/content/applications/Languages/PageHeader.tsx
+++ src/content/applications/Languages/PageHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Typography, Button, Grid, Dialog, DialogTitle, List, ListItem, ListItemAvatar, Avatar, ListItemText, Box } from '@mui/material';
+import { Typography, Button, Grid, Dialog, DialogTitle, List, ListItem, ListItemAvatar, Avatar, ListItemText, Box, OutlinedInput, InputAdornment, Stack } from '@mui/material';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
-import { AddIcCallOutlined, PersonPinCircleOutlined } from '@mui/icons-material';
+import { AddIcCallOutlined, PersonPinCircleOutlined, Search } from '@mui/icons-material';
 import PropTypes from 'prop-types';
 
 const emails = ["ABCDE", "DEFGH"];
@@ -18,8 +18,95 @@ function SimpleDialog(props) {
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
+    <Dialog
+      onClose={handleClose} open={open}>
+      <Box
+        sx={{
+          "width": "369px",
+          "height": "510px",
+          "background": "linear-gradient(139deg, #0E8A74 23.19%, #26C58B 104.35%)",
+          "position": "relative",
+          "overflow": "hidden"
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            width: "255.429px",
+            height: "427.169px",
+            top: "-100px",
+            right: "-100px",
+            flexShrink: 0,
+            borderRadius: "100%",
+            background: "rgba(205, 255, 242, 0.10)"
+          }}
+        ></Box>
+        <Box sx={{
+          position: "absolute",
+          top: "33px",
+          left: "30px"
+        }}
+        ><img src="/static/images/languages/wave.png" width={"35px"} /></Box>
+        <Box sx={{
+          position: "absolute",
+          bottom: "30px",
+          right: "27px"
+        }}
+        ><img src="/static/images/languages/vector.png" width={"35px"}/></Box>
+        <Box sx={{
+          position: "absolute",
+          top: "35px",
+          left: "120px"
+        }}>
+          <Typography fontFamily={"Poppins"} fontWeight={"500"} fontSize={"17px"} color={"white"}>
+            Add a language
+          </Typography>
+        </Box>
+        <Box padding={"40px"} paddingTop={"100px"} textAlign={"center"}>
+          <Stack direction={"column"} spacing={2}>
+            <OutlinedInput fullWidth
+              sx={{
+                "& fieldset": { border: 'none' },
+                background: "white",
+                padding: "5px"
+              }}
+              id="outlined-adornment-amount"
+              placeholder='Language'
+            />
+            <OutlinedInput fullWidth
+              sx={{
+                "& fieldset": { border: 'none' },
+                background: "white",
+                padding: "5px"
+              }}
+              id="outlined-adornment-amount"
+              placeholder='URL'
+            />
+            <OutlinedInput fullWidth
+              sx={{
+                "& fieldset": { border: 'none' },
+                background: "white",
+                padding: "5px"
+              }}
+              id="outlined-adornment-amount"
+              placeholder='Username'
+            />
+            <OutlinedInput fullWidth
+              sx={{
+                "& fieldset": { border: 'none' },
+                background: "white",
+                padding: "5px"
+              }}
+              id="outlined-adornment-amount"
+              placeholder='Password'
+            />
+          </Stack>
+          <Box paddingTop={"33px"}>
+            <Button sx={{width: "200px"}} variant='contained' color='primary'>Add</Button>
+          </Box>
+        </Box>
+      </Box>
+      {/* <DialogTitle>Set backup account</DialogTitle>
       <List sx={{ pt: 0 }}>
         {emails.map((email) => (
           <ListItem
@@ -46,7 +133,7 @@ function SimpleDialog(props) {
           </ListItemAvatar>
           <ListItemText primary="Add account" />
         </ListItem>
-      </List>
+      </List> */}
     </Dialog>
   );
 }
