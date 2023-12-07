7bc2af5ab0126b3e8c0f2ca66aea6c1b7eddf272
maradox49 <maradox49ss@gmail.com>
2023-12-07 22:52:31 +0100

    title changed

--- src/content/overview/index.tsx
+++ src/content/overview/index.tsx
@@ -18,7 +18,7 @@ function Overview() {
   return (
     <OverviewWrapper>
       <Helmet>
-        <title>Tokyo Free White React Typescript Admin Dashboard</title>
+        <title>Blog Automation of Singlequiver Surf Shop</title>
       </Helmet>
       <Container maxWidth="lg">
         <Box display="flex" justifyContent="center" py={5} alignItems="center">
