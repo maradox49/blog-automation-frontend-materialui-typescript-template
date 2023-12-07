5eedf0eb0481ff256c7d0ae7e023096b610dcfa3
maradox49 <maradox49ss@gmail.com>
2023-12-07 15:12:34 +0100

    language context added

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,1 +1,37 @@
-export default "language";
+import { FC, useState, createContext } from 'react';
+import { LanguageType } from 'src/models/language';
+import { getAllLanguageService } from 'src/services/Language';
+type LanguageContext = {
+    languages: LanguageType[],
+    load: () => void,
+    add: (LanguageType) => void,
+    delete: (string) => void,
+    edit: (string, LanguageType) => void
+};
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const LanguageContext = createContext<LanguageContext>(
+  {} as LanguageContext
+);
+
+export const LanguageProvider: FC = ({ children }) => {
+    const [languages, setLanguages] = useState<LanguageType[]>([]);
+
+    const load = async () => {
+        try {
+            const responseData = await getAllLanguageService();
+            if ( responseData ) {
+                setLanguages(responseData);
+            }
+        } catch (error) {
+        }
+    }
+
+  return (
+    <LanguageContext.Provider
+      value={{ isLoading, loadingMessage, startLoading, stopLoading }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
