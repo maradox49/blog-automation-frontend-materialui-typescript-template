6d47899dd6767163fb1d43fc315b6f2b1d6c0b53
maradox49 <maradox49ss@gmail.com>
2023-12-10 15:53:46 +0100

    backend url changed

--- src/config/Global.ts
+++ src/config/Global.ts
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: process.env.NODE_ENV === "production" ? "http://172.86.99.237:4004/api" 
+    baseUrl: process.env.NODE_ENV === "production" ? "http://automation.singlequiver.com:4004/api" 
     : "http://automation.singlequiver.com:4004/api",
     sourceBlogUrl: "https://www.singlequiver.com/enelpico/wp-json/wp/v2",
 };
